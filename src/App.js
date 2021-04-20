1fa2a48d7a5caa1c1e260dd52e5c8a2c6e96b231
ganett33 <65261313+ganett33@users.noreply.github.com>
2021-04-20 16:00:54 +1200

    #4.3 Adding Genres

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie"
+import"./App.css";
 
 //best way to use setState that not depends on that external state.
 class App extends React.Component {
@@ -28,22 +29,28 @@ class App extends React.Component {
   render() {
     const { isLoding, movies } = this.state;
     return (
-      <div>
+      <section className="container">
         {isLoding 
-        ? "Loading..." 
+        ? <div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div> 
         : movies.map(movie => (
-              <Movie 
+          <div className="movies">
+            <Movie 
                 key ={movie.id}
                 id={movie.id}
                 year={movie.year}
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
                />
+          </div>
+              
             )
           )
         }      
-      </div>
+      </section>
     );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({id, title, summary, poster, year}) {
-    return <h4>{title}</h4>
+function Movie({title, summary, poster, year, genres}) {
+    return (
+        <div className="movies__movie">
+            <img src={poster} alt={title} title={title} />
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="genres">
+                    {genres.map((genres, index) => (
+                    <li key ={index} className="genres__genre">{genres}</li>
+                    ))}
+                    </ul>
+                <p className="movie__summary">{summary}</p>
+            </div>
+        </div>
+    ) 
 }
 
 Movie.prototype = {
@@ -10,9 +25,8 @@ Movie.prototype = {
         title: PropTypes.string.isRequired,
         summary: PropTypes.string.isRequired,
         poster: PropTypes.string.isRequired,
-        year: PropTypes.string.isRequired
-
-
+        year: PropTypes.string.isRequired,
+        genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export default Movie;
